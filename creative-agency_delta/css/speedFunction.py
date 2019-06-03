2287e0f9481e2a8cbe0aaed771fcaa4cb020890a
SeledousSystems <applevillas@gmail.com>
2019-06-03 21:08:24 +1200

    Add files via upload

---
+++ creative-agency_delta/css/speedFunction.py
@@ -0,0 +1,20 @@
+print ("Hello, I am {} years old !".format(18)) 
+
+def calcSpeed(distance, time): 
+    speed = distance/time
+    return speed;
+
+
+
+speed = calcSpeed(500, 7)
+print(speed)
+speed = calcSpeed(100, 9)
+print(speed)
+speed = calcSpeed(500, 6)
+print(speed)
+speed = calcSpeed(500, 78)
+print(speed)
+speed = calcSpeed(500, 0)
+print(speed)
+speed = calcSpeed(500, 5000)
+print(speed)
